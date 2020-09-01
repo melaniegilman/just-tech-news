ac57a3775a9544fc121452c82441090712b56a24
Melanie Gilman <melaniegilman259@yahoo.com>
2020-09-01 18:00:26 -0400

    logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,18 +58,33 @@ router.get('/:id', (req, res) => {
 
 
 // POST /api/users
-router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
 });
 
 // PUT /api/users/1
@@ -113,4 +128,34 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+// POST /api/users
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+      username: req.body.username, // key/value pairs .... keys are defined in User.js
+      email: req.body.email,
+      password: req.body.password
+  })
+      .then(dbUserData => { // this is a callback method
+          req.session.save(() => { // gives server access to user_id and username
+              req.session.user_id = dbUserData.id;
+              req.session.username = dbUserData.username;
+              req.session.loggedIn = true;
+              res.json(dbUserData);
+          });
+      })
+});
+
+// POST /logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+      req.session.destroy(() => {
+          res.status(204).end(); // successful destruction
+      });
+  }
+  else {
+      res.status(404).end();
+  }
+});
 module.exports = router;
+
--- server.js
+++ server.js
@@ -2,25 +2,37 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+// This sets up HANDLEBARS.js HTML template engine
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+// add in css stylesheet
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
-// turn on connection to database and server
-sequelize.sync({ force: false }).then(() => {
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => { // sequelize taking the models and connecting them to associated database tables
   app.listen(PORT, () => console.log('Now listening'));
 });
+// if force was set to true it would drop and re-create all the database tables on startup
+// force true will make tables re-create in there are any association changes
